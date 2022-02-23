f27ca9e9d214c6cc49c4ae6b0ecd4d11bf41a3f6
Benoît Hubert <benoithubert@gmail.com>
2022-02-23 22:18:21 +0100

    Initialize from CRA/TS/ESLint/Prettier template

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+};
